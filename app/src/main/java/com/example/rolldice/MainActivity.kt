c60635562988a5c7c0dee8f4b05a90bf00f49849
Mar <mal7othify@gmail.com>
2020-12-17 03:58:02 +0300

    upgrade to compose alpha09

--- app/src/main/java/com/example/rolldice/MainActivity.kt
+++ app/src/main/java/com/example/rolldice/MainActivity.kt
@@ -2,10 +2,10 @@ package com.example.rolldice
 
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.ui.platform.setContent
 import com.example.rolldice.ui.RollDiceTheme
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/rolldice/Screen.kt
+++ app/src/main/java/com/example/rolldice/Screen.kt
@@ -13,8 +13,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.AmbientContext
-import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,12 +25,13 @@ import com.example.rolldice.ui.typography
 fun Screen() {
     Scaffold(
         topBar = {
-            TopAppBar {
-                Text(
-                    "Roll Dice Application",
-                    modifier = Modifier.align(Alignment.CenterVertically)
-                )
-            }
+            TopAppBar(
+                title = {
+                    Text(
+                        "Roll Dice Application"
+                    )
+                }
+            )
         }
     ) {
         Content()
@@ -39,15 +40,15 @@ fun Screen() {
 
 @Composable
 fun Content() {
-    val context = AmbientContext.current
+    val context = LocalContext.current
     val rollState = remember { mutableStateOf(6) }
     val asset = when (rollState.value) {
-        1 -> imageResource(R.drawable.dice_1)
-        2 -> imageResource(R.drawable.dice_2)
-        3 -> imageResource(R.drawable.dice_3)
-        4 -> imageResource(R.drawable.dice_4)
-        5 -> imageResource(R.drawable.dice_5)
-        else -> imageResource(R.drawable.dice_6)
+        1 -> ImageBitmap.imageResource(R.drawable.dice_1)
+        2 -> ImageBitmap.imageResource(R.drawable.dice_2)
+        3 -> ImageBitmap.imageResource(R.drawable.dice_3)
+        4 -> ImageBitmap.imageResource(R.drawable.dice_4)
+        5 -> ImageBitmap.imageResource(R.drawable.dice_5)
+        else -> ImageBitmap.imageResource(R.drawable.dice_6)
     }
 
     Column(
@@ -55,9 +56,9 @@ fun Content() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(asset, modifier = Modifier.size(200.dp))
+        Image(asset, modifier = Modifier.width(160.dp).height(200.dp), contentDescription = null)
         Text(rollState.value.toString(), style = typography.h3, color = Color.Gray)
-        Spacer(modifier = Modifier.preferredHeight(16.dp))
+        Spacer(modifier = Modifier.requiredHeight(16.dp))
         Button(
             onClick = {
                 roll(
